cf2c59c672c7c4fed55e37fddaf775321454c231
Maria <msalamaha97@ukr.net>
2020-12-06 23:52:09 +0200

    fixed function parameters

--- index.js
+++ index.js
@@ -1,63 +1,56 @@
-function getMaxDigit() {
-    const valueFromUser = prompt(`Введіть багатозначне число`);
-    const arrayResult = [];
-    for (i = 0; i < valueFromUser.length; i++) {
-        arrayResult.push(parseInt(valueFromUser[i]));
-    }
-    return Math.max(...arrayResult);
-}
+function getMaxDigit(number) {
 
-function convertToCapitalize() {
-  let userName = prompt(`Введіть своє ім'я`);
-  userName = userName.charAt(0).toUpperCase() + userName.slice(1).toLowerCase(); 
-
-  return userName;
-}
-
-function calculateTheTax() {
-    const salaryValue = prompt(`Вкажіть суму заробітньої плати`);
-    const tax = 0.195;
-    return  parseFloat(salaryValue) - parseFloat(salaryValue) * tax;
-}
-
-function getRandomNumber () {
-    const min = prompt(`Введіть початкове число`);
-    const max = prompt(`Введіть кінцеве число`);
-    return Math.floor(Math.random() * (Math.ceil(parseFloat(max)) -  Math.ceil(parseFloat(min)) + 1)) +  Math.ceil(parseFloat(min)) ;
-}
-
-function countLetter() {
-    let inputWord = prompt(`Введіть будь-яке слово`);
-    let countLetterA = inputWord.toLowerCase().split("a").length - 1;
+    const valueFromUser = Array.from(number.toString()).map(Number); 
     
-    return countLetterA;
-    
-}
-
-function convertCurrency() {
-    const userAmount = prompt(`Введіть суму для конвертування (зі знаком $ або UAH)`);
-    const dollarCurrency = 28.3;
-    const dollar = userAmount.includes("$");
-    const hryvnia = userAmount.includes("UAH");
-
-    if (dollar) {
-        const number = parseFloat(userAmount);
-        const value = Math.random((number * dollarCurrency) + 'UAH');
-        return value;
-    } else if (hryvnia) {
-        const number = parseFloat(userAmount);
-        const value = Math.round((number / dollarCurrency) + '$');
-        return value;
-    } else {
-        alert(`Введіть суму зі знаком $ або UAH`);
-    }
-    
-}
-
-document.writeln(`Функція №1: ${getMaxDigit()} <br>`);
-document.writeln(`Функція №2: ${convertToCapitalize()} <br>`);
-document.writeln(`Функція №3: ${calculateTheTax()} <br>`);
-document.writeln(`Функція №4: ${getRandomNumber()} <br>`);
-document.writeln(`Функція №5: ${countLetter()} <br>`);
-document.writeln(`Функція №6: ${convertCurrency()} <br>`);
-
+    return Math.max(...valueFromUser); 
+ }
+ 
+ function convertToCapitalize(userName) {
+ 
+   userName = userName.charAt(0).toUpperCase() + userName.slice(1).toLowerCase(); 
+ 
+   return userName;
+ }
+ 
+ function calculateTheTax(salaryValue) {
+     const tax = 0.195;
+     return  parseFloat(salaryValue) - parseFloat(salaryValue) * tax;
+ }
+ 
+ function getRandomNumber(min, max) {
+ 
+     return Math.floor(Math.random() * (Math.ceil(parseFloat(max)) -  Math.ceil(parseFloat(min)) + 1)) +  Math.ceil(parseFloat(min)) ;
+ }
+ 
+ function countLetter(inputWord) {
+     let countLetterA = inputWord.toLowerCase().split("a").length - 1;
+     
+     return countLetterA;
+     
+ }
+ 
+ function convertCurrency(userAmount) {
+     const dollarCurrency = 28.3;
+     const dollar = userAmount.includes('$');
+     const hryvnia = userAmount.includes('UAH');
+ 
+     if (dollar) {
+         const number = parseFloat(userAmount);
+         const value = Math.round(number * dollarCurrency) + 'UAH';
+         return value;
+     } else if (hryvnia) {
+         const number = parseFloat(userAmount);
+         const value = Math.round(number / dollarCurrency) + '$';
+         return value;
+     } else {
+         alert('Введіть суму зі знаком $ або UAH');
+     }
+     
+ }
+ 
+ document.writeln(`Функція №1: ${getMaxDigit(456)} <br>`);
+ document.writeln(`Функція №2: ${convertToCapitalize('olEna')} <br>`);
+ document.writeln(`Функція №3: ${calculateTheTax(15000)} <br>`);
+ document.writeln(`Функція №4: ${getRandomNumber(10, 200)} <br>`);
+ document.writeln(`Функція №5: ${countLetter('America')} <br>`);
+ document.writeln(`Функція №6: ${convertCurrency('588$')} <br>`);
